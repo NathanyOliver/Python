c916450101dc850cefa4523c010868d24dd71d33
NathanyOliver <143663238+NathanyOliver@users.noreply.github.com>
2023-09-25 21:51:42 -0300

    Add files via upload

---
+++ Nathy/primeiro_arq.py
@@ -0,0 +1,6 @@
+#print("Ola, Mundo!")
+#Esse é o meu primeiro algoritmo
+nome = "Hélder"
+print("Olá, {}" .format(nome))
+print(f"Olá, {nome}")
+
---
+++ Nathy/string.py
@@ -0,0 +1,8 @@
+nome = str(input("Digite seu nome: "))
+sobrenome = input("Digite seu sobrenome: ")
+
+print("Tipo da variavel nome:", type(nome))
+print("Tipo da variavel sobrenome:", type(sobrenome))
+print(f"Nome e Sobrenome: {nome}{sobrenome}")
+print("O numero de letras do nome eh: " , len(nome))
+print("O numero de letras do sobrenome eh: " , len(sobrenome))
