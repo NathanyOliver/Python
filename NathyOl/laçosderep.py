0c8b8d998e01b9ef99c51fcd5165ba6fcb0bed25
NathanyOliver <143663238+NathanyOliver@users.noreply.github.com>
2023-10-05 21:41:40 -0300

    Add files via upload

---
+++ NathyOl/laçosderep.py
@@ -0,0 +1,33 @@
+#x = 0
+
+#while x < 5:
+ #   nome = input ('Qual é o seu nome? ')
+  #  x=x+1
+  
+  #Atividade 2
+  
+#lista_compras = []
+#quantidade = 0
+
+#while quantidade <5:
+ #   item_lista = input("Qual item vc quer adicionar a lista? ")
+  #  lista_compras.append(item_lista)
+   # quantidade=quantidade+1
+    
+#print("a minha lista de compras final: ", lsita_compras )  
+  
+  #Atividade 3
+  
+#faturamento = [100, 250, 220, 500, 2000]
+#total = 0
+
+#for i in faturamento:
+ #   total = total + i
+      
+#print(total) 
+
+#Atividade 4
+
+for i in range(0,11):
+    #neste looping mostrará os  números ordenaoda de 0 até 10
+    print(i)        
